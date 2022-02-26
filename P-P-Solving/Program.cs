a4d8c8d625da6eec7dc9cbca11adfa3fd4d9911e
Udara% <pubuduudara7.pu@gmail.com>
2022-02-26 21:37:48 +0530

    Add project files.

---
+++ P-P-Solving/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace P_P_Solving
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+          
+            Q_1 q_1 = new Q_1();
+            q_1.ToUpper();
+
+			}
+    }
+}
---
+++ P-P-Solving/Q-1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P_P_Solving
+{
+    class Q_1
+    {
+		//Write a program that converts 1 lower case letter("a" - "z") to its corresponding upper case letter("A" - "Z"). 
+		//	For example if the user enters "c" then the program will show "C" on the screen.
+
+		public void ToUpper()
+		{
+			char x;
+			int y;
+			Console.WriteLine("Enter Latter- ");
+			x = Convert.ToChar(Console.ReadLine());
+			y= (int)x;
+			if (y >= 97 && y <= 122)
+			{
+				y = y - 32;
+				x = (char)y;
+				
+				Console.WriteLine("Uppercase Letter is = " + x);
+			}
+			
+		}
+    }
+}
