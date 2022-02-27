a49a33b5bf65d7fb4d08df5ecc7442510df05f60
Udara% <pubuduudara7.pu@gmail.com>
2022-02-27 22:33:44 +0530

    Day27

--- P-P-Solving/Program.cs
+++ P-P-Solving/Program.cs
@@ -9,7 +9,9 @@ namespace P_P_Solving
             Console.WriteLine("Hello World!");
           
             Q_1 q_1 = new Q_1();
-            q_1.ToUpper();
+            // q_1.ToUpper();
+
+            q_1.FirstFactor();
 
 			}
     }
--- P-P-Solving/Q-1.cs
+++ P-P-Solving/Q-1.cs
@@ -8,6 +8,7 @@ namespace P_P_Solving
 {
     class Q_1
     {
+		//Qs-1
 		//Write a program that converts 1 lower case letter("a" - "z") to its corresponding upper case letter("A" - "Z"). 
 		//	For example if the user enters "c" then the program will show "C" on the screen.
 
@@ -27,5 +28,25 @@ namespace P_P_Solving
 			}
 			
 		}
-    }
+
+		//Qs-2
+		//Have  function FirstFactorial(number) take  parameter is passed and return the factorial of 
+		//it (ex: if num = 4, return (4 * 3 * 2 * 1)). For  test cases,  range will  between 1 and 18 and the 
+		//input will always  integer one.
+
+		public  void FirstFactor()
+        {
+			
+			Console.WriteLine("Enter Number- ");
+			int number = Convert.ToInt32(Console.ReadLine());
+			int result = 1;
+			for (int i = number; i > 0; i--)
+            {
+				result *= 1;
+
+            }
+			Console.WriteLine("Result is = " + result);
+		}
+
+	}
 }
